e839e6be220ccf9175d2463625c3af4b23396424
Samyr Marjoub <96318513+SamyrMarjoub@users.noreply.github.com>
2022-07-21 23:13:53 -0300

    Add files via upload

--- App.js
+++ App.js
@@ -8,21 +8,90 @@ class App extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      itens: [],
+      valores: [],
       detalhes: '',
       valor: '',
       tipo: '',
-      id: ''
+      uid: 0,
+      entradaTotal: 0,
+      saidaTotal: 0,
+      Total: 0
 
     }
     this.submit = this.submit.bind(this)
     this.desc = this.desc.bind(this)
     this.value = this.value.bind(this)
     this.radio = this.radio.bind(this)
+    this.cor = this.cor.bind(this)
   }
 
   submit(e) {
-    
+    this.setState({ uid: ++this.state.uid })
+    e.preventDefault()
+    const conteudo = document.querySelector('.flex-direction')
+    const Maincontainer = document.createElement('div')
+    Maincontainer.classList.add('historic-container-info')
+    Maincontainer.id = this.state.uid
+    const Secondcontainer = document.createElement('div')
+    Secondcontainer.classList.add('cnt', 'top')
+    const m1 = document.createElement('div')
+    m1.classList.add('first-d', 'flex')
+    const m2 = document.createElement('div')
+    m2.classList.add('second-d', 'flex')
+    const m3 = document.createElement('div')
+    m3.classList.add('td-d', 'flex')
+    const m4 = document.createElement('div')
+    m4.classList.add('last-d', 'flex')
+    const m1span = document.createElement('span')
+    const m2span = document.createElement('span')
+    const m3div = document.createElement('div')
+    const m3img = document.createElement('img')
+    m3img.classList.add('img-act')
+    const m4img = document.createElement('img')
+    m4img.classList.add('trash-img')
+    conteudo.append(Maincontainer)
+    Maincontainer.append(Secondcontainer)
+    Secondcontainer.append(m1, m2, m3, m4)
+    m1.append(m1span)
+    m2.append(m2span)
+    m3.append(m3div)
+    m3div.append(m3img)
+    m4.append(m4img)
+    m1span.innerHTML = this.state.detalhes
+    m2span.innerHTML = `R$: ${this.state.valor},00`
+    m4img.src = trash
+    m4img.addEventListener('click', function () {
+      document.getElementById(Maincontainer.id).remove()
+    })
+    if (this.state.tipo === 'saida') {
+      m3div.classList.add('div-img-act-red')
+      m3img.src = down
+      this.setState({ saidaTotal: parseFloat(this.state.valor) + parseFloat(this.state.saidaTotal) })
+    } else if (this.state.tipo === 'entrada') {
+      m3div.classList.add('div-img-act')
+      m3img.src = up
+      this.setState({ entradaTotal: parseFloat(this.state.valor) + parseFloat(this.state.entradaTotal) })
+    }
+    if (this.state.tipo === 'entrada') {
+      this.setState({ Total: parseFloat(this.state.valor) + parseFloat(this.state.Total) })
+    } else if (this.state.tipo === 'saida') {
+      this.setState({ Total: parseFloat(this.state.Total) - parseFloat(this.state.valor) })
+    }
+
+    const objeto = {
+      id: Date.now(),
+      valor: this.state.valor
+    }
+    setTimeout(() => {
+      console.log(this.state.valores)
+    }, 100);
+    // this.setState({valores: [...this.state.valores, objeto]})
+    this.setState({ valores: this.state.valores.concat(objeto) })
+    this.setState({ detalhes: '' })
+    this.setState({ valor: '' })
+  }
+  componentDidUpdate() {
+    this.cor()
   }
   value(e) {
     this.setState({ valor: e.target.value })
@@ -32,6 +101,13 @@ class App extends Component {
   }
   radio(e) {
     this.setState({ tipo: e.target.value })
+  }
+  cor() {
+    if (this.state.Total < 0) {
+      document.querySelector('.total').classList.add('prejuizo')
+    } else {
+      document.querySelector('.total').classList.remove('prejuizo')
+    }
   }
 
   render() {
@@ -58,7 +134,7 @@ class App extends Component {
                     <div className='on2'> <div className='div-img-act'> <img src={up} alt='ok' id='img-act' />  </div> </div>
                   </div>
                   <div className='score'>
-                    <h2>R$0.00</h2>
+                    <h2>R$ {this.state.entradaTotal}.00</h2>
                   </div>
                 </div>
               </div>
@@ -69,7 +145,7 @@ class App extends Component {
                     <div className='on2'> <div className='div-img-act-red'> <img src={down} alt='ok' id='img-act' />  </div> </div>
                   </div>
                   <div className='score'>
-                    <h2>R$0.00</h2>
+                    <h2>R$ {this.state.saidaTotal}.00</h2>
                   </div>
                 </div>
               </div>
@@ -80,7 +156,7 @@ class App extends Component {
                     <div className='on2'> <div className='div-img-act-w'> <img src={dollar} alt='ok' id='img-act' />  </div> </div>
                   </div>
                   <div className='score'>
-                    <h2>R$0.00</h2>
+                    <h2 className='total'>R$ {this.state.Total}.00</h2>
                   </div>
                 </div>
               </div>
@@ -131,14 +207,10 @@ class App extends Component {
                   <div className='last-d'> <span>Remover</span></div>
                 </div>
               </div>
-              <div className='historic-container-info'>
-                <div className='cnt top'>
-                  <div className='first-d flex'> <span>0</span> </div>
-                  <div className='second-d flex'> <span>R$:2.00</span> </div>
-                  <div className='td-d flex'> <div className='div-img-act'> <img src={up} alt='ok' id='img-act' />  </div></div>
-                  <div className='last-d flex'> <img src={trash} id='trash-img' alt='..' ></img></div>
-                </div>
+              <div className='flex-direction'>
+
               </div>
+
             </div>
           </div>
         </main>
