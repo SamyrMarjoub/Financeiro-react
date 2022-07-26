3e3770abf7c4451de17fed1d9c5581ed28b17ac9
Samyr Marjoub <96318513+SamyrMarjoub@users.noreply.github.com>
2022-07-25 23:46:26 -0300

    Add files via upload

--- App.js
+++ App.js
@@ -4,34 +4,45 @@ import dollar from './images/dollar.svg'
 import up from './images/up.svg'
 import down from './images/down.svg'
 import trash from './images/trash.svg'
+// import Tela from './components/app'
 class App extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      valores: [],
+      storagerray: [],
       detalhes: '',
       valor: '',
       tipo: '',
       uid: 0,
       entradaTotal: 0,
       saidaTotal: 0,
-      Total: 0
+      Total: 0,
+      uidcT: 0,
+      getitens: JSON.parse(localStorage.getItem('itens'))
 
     }
     this.submit = this.submit.bind(this)
-    this.desc = this.desc.bind(this)
-    this.value = this.value.bind(this)
-    this.radio = this.radio.bind(this)
     this.cor = this.cor.bind(this)
+    this.controleDeSaida = this.controleDeSaida.bind(this)
+
+    // if (window.performance) {
+    //   if (performance.navigation.type === 1) {
+    //     localStorage.setItem('boolean', true)
+    //   }else{
+    //     localStorage.setItem('boolean', false)
+    //   }
+    // }
+
   }
 
   submit(e) {
+
     this.setState({ uid: ++this.state.uid })
     e.preventDefault()
     const conteudo = document.querySelector('.flex-direction')
     const Maincontainer = document.createElement('div')
-    Maincontainer.classList.add('historic-container-info')
-    Maincontainer.id = this.state.uid
+    Maincontainer.classList.add('historic-container-info', 'aaa')
+    Maincontainer.id = `M${this.state.uid}`
     const Secondcontainer = document.createElement('div')
     Secondcontainer.classList.add('cnt', 'top')
     const m1 = document.createElement('div')
@@ -58,49 +69,84 @@ class App extends Component {
     m3div.append(m3img)
     m4.append(m4img)
     m1span.innerHTML = this.state.detalhes
-    m2span.innerHTML = `R$: ${this.state.valor},00`
     m4img.src = trash
-    m4img.addEventListener('click', function () {
-      document.getElementById(Maincontainer.id).remove()
-    })
+    m4img.id = `I${this.state.uidcT}`
+
+    if (this.state.tipo === 'saida') {
+      m2span.innerHTML = `RS$: <span class=T${this.state.uidcT}>${this.state.valor}</span>.00`
+    } else if (this.state.tipo === 'entrada') {
+      m2span.innerHTML = `RS$: <span class=T${this.state.uidcT}>${this.state.valor}</span>.00`
+    }
+
     if (this.state.tipo === 'saida') {
       m3div.classList.add('div-img-act-red')
       m3img.src = down
       this.setState({ saidaTotal: parseFloat(this.state.valor) + parseFloat(this.state.saidaTotal) })
+      Maincontainer.classList.add('saida')
     } else if (this.state.tipo === 'entrada') {
       m3div.classList.add('div-img-act')
       m3img.src = up
       this.setState({ entradaTotal: parseFloat(this.state.valor) + parseFloat(this.state.entradaTotal) })
+      Maincontainer.classList.add('entrada')
+
     }
+    m2.classList.add(this.state.uidcT)
+    this.setState({ uidcT: ++this.state.uidcT })
+
+
     if (this.state.tipo === 'entrada') {
       this.setState({ Total: parseFloat(this.state.valor) + parseFloat(this.state.Total) })
     } else if (this.state.tipo === 'saida') {
       this.setState({ Total: parseFloat(this.state.Total) - parseFloat(this.state.valor) })
     }
+    const imgdinamica = document.getElementById(m4img.id)
+    imgdinamica.addEventListener('click', () => {
+      if (document.getElementById(Maincontainer.id).classList.contains('saida')) {
+        let conta = m4img.id
+        const node = document.querySelector(`.T${conta[1]}`)
+        let valor = node.innerText
+        this.setState({ saidaTotal: parseFloat(this.state.saidaTotal) - parseFloat(valor) })
+        this.setState({ Total: parseFloat(this.state.Total) + parseFloat(valor) })
+        document.getElementById(Maincontainer.id).remove()
+      } else if (document.getElementById(Maincontainer.id).classList.contains('entrada')) {
+        let conta = m4img.id
+        const node = document.querySelector(`.T${conta[1]}`)
+        let valor = node.innerText
+        this.setState({ entradaTotal: parseFloat(this.state.entradaTotal) - parseFloat(valor) })
+        this.setState({ Total: parseFloat(this.state.Total) - parseFloat(valor) })
+        document.getElementById(Maincontainer.id).remove()
+      }
+
+    })
 
     const objeto = {
-      id: Date.now(),
-      valor: this.state.valor
+      idM: this.state.uid,
+      idT: this.state.uidcT,
+      detalhes: this.state.detalhes,
+      valor: this.state.valor,
+      tipo: this.state.tipo
     }
-    setTimeout(() => {
-      console.log(this.state.valores)
-    }, 100);
-    // this.setState({valores: [...this.state.valores, objeto]})
-    this.setState({ valores: this.state.valores.concat(objeto) })
+    // this.setState({ storagerray: this.state.storagerray.concat(objeto) })
+    // localStorage.setItem('itens', JSON.stringify(this.state.storagerray))
     this.setState({ detalhes: '' })
     this.setState({ valor: '' })
   }
-  componentDidUpdate() {
-    this.cor()
-  }
-  value(e) {
-    this.setState({ valor: e.target.value })
-  }
-  desc(e) {
-    this.setState({ detalhes: e.target.value })
-  }
-  radio(e) {
-    this.setState({ tipo: e.target.value })
+
+
+  componentDidUpdate(prevPros, prevState) {
+    if (prevState.Total !== this.state.Total) {
+      this.cor()
+      this.controleDeSaida()
+      localStorage.setItem('itens', JSON.stringify(this.state.storagerray))
+      console.log(localStorage.getItem('itens'))
+    }
+    if (prevState.visitedd !== this.state.visitedd) {
+
+    }
+    if (prevState.storagerray !== this.state.storagerray) {
+
+    }
+
   }
   cor() {
     if (this.state.Total < 0) {
@@ -109,8 +155,14 @@ class App extends Component {
       document.querySelector('.total').classList.remove('prejuizo')
     }
   }
+  controleDeSaida() {
+    if (this.state.saidaTotal < 0) {
+      this.setState({ saidaTotal: 0 })
+    }
+  }
 
   render() {
+
     return (
       <div>
         <header className='header'>
@@ -134,7 +186,7 @@ class App extends Component {
                     <div className='on2'> <div className='div-img-act'> <img src={up} alt='ok' id='img-act' />  </div> </div>
                   </div>
                   <div className='score'>
-                    <h2>R$ {this.state.entradaTotal}.00</h2>
+                    <h2>R$ <span className='nme'>{this.state.entradaTotal}</span>.00</h2>
                   </div>
                 </div>
               </div>
@@ -144,8 +196,8 @@ class App extends Component {
                     <div className='on1'> <h3>Saidas</h3> </div>
                     <div className='on2'> <div className='div-img-act-red'> <img src={down} alt='ok' id='img-act' />  </div> </div>
                   </div>
-                  <div className='score'>
-                    <h2>R$ {this.state.saidaTotal}.00</h2>
+                  <div className='score saida-h2'>
+                    <h2>R$ <span className='nmv'>{this.state.saidaTotal}</span>.00</h2>
                   </div>
                 </div>
               </div>
@@ -156,7 +208,7 @@ class App extends Component {
                     <div className='on2'> <div className='div-img-act-w'> <img src={dollar} alt='ok' id='img-act' />  </div> </div>
                   </div>
                   <div className='score'>
-                    <h2 className='total'>R$ {this.state.Total}.00</h2>
+                    <h2 className='total'>R$ <span className='nmt'> {this.state.Total}</span>.00</h2>
                   </div>
                 </div>
               </div>
@@ -166,18 +218,18 @@ class App extends Component {
                 <div className='input-container'>
                   <div className='flexdiv'>
                     <div className='inputs-div margin'><span>Descricão</span>
-                      <input type={'text'} className='input-txt' onChange={this.desc} required={true} value={this.state.detalhes} />
+                      <input type={'text'} className='input-txt' onChange={(e) => { this.setState({ detalhes: e.target.value }) }} required={true} value={this.state.detalhes} />
                     </div>
                     <div className='inputs-div'><span>Valor</span>
-                      <input type={'number'} className='input-txt' placeholder='R$' onChange={this.value} value={this.state.valor} required />
+                      <input type={'number'} className='input-txt' placeholder='R$' onChange={(e) => { this.setState({ valor: e.target.value }) }} value={this.state.valor} required />
                     </div>
                   </div>
                   <div className='flexdiv'>
                     <div className='select-div'>
                       <div className='radio-div'>
-                        <input type={'radio'} id='entrada' name='entrada' value={'entrada'} className='radio' onChange={this.radio} required />
+                        <input type={'radio'} id='entrada' name='entrada' value={'entrada'} className='radio' onChange={(e) => { this.setState({ tipo: e.target.value }) }} required />
                         <label id='margin' htmlFor='entrada'>Entrada</label>
-                        <input type={'radio'} id='saida' name='entrada' value={'saida'} className='radio' onChange={this.radio} required />
+                        <input type={'radio'} id='saida' name='entrada' value={'saida'} className='radio' onChange={(e) => { this.setState({ tipo: e.target.value }) }} required />
                         <label htmlFor='saida'>Saida</label>
                         <input type={'submit'} value='Adicionar' className='btn' />
                       </div>
@@ -185,7 +237,6 @@ class App extends Component {
                   </div>
                 </div>
               </form>
-
             </div>
             <div className='filter-div'>
               <span id='filter-span'>Filtrar:</span>
@@ -208,14 +259,25 @@ class App extends Component {
                 </div>
               </div>
               <div className='flex-direction'>
-
+                {localStorage.getItem('itens') && localStorage.getItem('boolean') ? <div className='historic-container-info aaa' id={this.state.uid}>{this.state.getitens.map((e) => {
+                  return (
+                    <div className='cnt top' key={e.idM}>
+                      <div className='first-d flex'> <span>{e.detalhes}</span> </div>
+                      <div className='second-d flex'> <span>R${e.valor}.00</span> </div>
+                      <div className='td-d flex'> <div className={e.tipo === 'entrada' ? 'div-img-act' : 'div-img-act-red'}> <img src={e.tipo === 'entrada' ? up : down} className='img-act' alt='' /> </div> </div>
+                      <div className='last-d flex'> <img src={trash} className='trash-img' alt='' /> </div>
+                    </div>
+                  )
+                })} </div> : <></>}
               </div>
-
             </div>
           </div>
         </main>
       </div>
     )
   }
+
+
 }
+
 export default App
